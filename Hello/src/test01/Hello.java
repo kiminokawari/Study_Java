e71d9578c94d7b2b45e7989d41e4eeeed0b65924
kiminokawari <you@example.com>
2021-08-17 12:07:12 +0800

    练习创建后提交

--- Hello/src/test01/Hello.java
+++ Hello/src/test01/Hello.java
@@ -3,5 +3,9 @@ package test01;
 public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello,World.");
+        /*
+        1.熟悉git操作
+        2.掌握快捷操作psvm和sout
+         */
     }
 }
